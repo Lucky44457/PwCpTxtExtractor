fed7548cb2bb319298aa2ef1ef2bb8aa0a86887b
Lucky44457 <maribelwate@gmail.com>
2025-09-11 21:23:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "25377714"
-api_hash = "ffc0d0e9d25947127bbf344e83a43f8c"
-bot_token = "8232406162:AAFV5YDemQmMvFbGl6wZ8HOIdY4O2hm4MsA"
+api_id = "25"
+api_hash = "ffc0d0e9d2594bf344e83a43f8c"
+bot_token = "8232406162emQmMvFbGl6wZ8HOIdY4O2hm4MsA"
 auth_users = [7503380727]
